d4e904e1d7d8f94b1915cd61dd632af47f37b6fe
byamane <byamane21@gmail.com>
2022-02-02 17:08:12 -0600

    add reset button

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 const toDo = document.getElementById('to-do')
 const btn = document.getElementById('submit-button')
 const list = document.getElementById('todo-list')
+const reset = document.getElementById('reset-button')
 
-btn.addEventListener('click', function(evt) {
+btn.addEventListener('click', function() {
 // Create a new li element when the button is clicked
   const newLi = document.createElement('li')
 // Set text of new li to user's text from input element
   newLi.textContent = toDo.value
+// Add the new li element to the page inside the ul
+// Do not allow empty items to be added
+  if (toDo.value !== ""){
+  list.appendChild(newLi)
 // Reset the text in the input field to an empty string
   toDo.value = ''
-// Add the new li element to the page inside the ul
-  list.appendChild(newLi)
+  }
 })
+
+reset.addEventListener('click', function(){
+// When reset button is clicked, remove all items from the ul & clear the input field
+  list.textContent = ''
+  toDo.value = ''
+})
+
